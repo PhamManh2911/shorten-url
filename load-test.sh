73958358cdc8fb6ae539e75304f4374965b626db
phammanh2911 <phammanh2911@gmail.com>
2026-04-06 16:21:25 +0700

    change concurrency setting in load test

--- load-test.sh
+++ load-test.sh
@@ -1,1 +1,3 @@
-wrk -t10 -c400 -d20s --latency http://127.0.0.1:3000/shorten-url/rnnY20Xj
+# setting more reasonable concurrency setting for single instance service
+# 400 may worsen the accept queue cause long tail latency
+wrk -t10 -c200 -d20s --latency http://127.0.0.1:3000/shorten-url/rnnY20Xj
